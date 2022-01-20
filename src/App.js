bae1e4421f890656c02bec6e9ee98aa90dbac9a6
Heta Björklund <heta.bjorklund@gmail.com>
2022-01-20 20:48:26 +0200

    Kaikki toimii

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 import React, {useState, useEffect} from "react";
-//import { Dropdown, DropdownButton } from 'react-bootstrap';
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import jsondata from './assets/data.json';
-  
+
 function App() {
   
-  // taulukko kaikille haetuille tiedoille
+  // muuttuja kaikille haetuille tiedoille
   const [kaikki, setKaikki] = useState([]);
 
+  // onko maan valinta tehty ja taulukko näytetään
   const [valmis, setValmis] = useState(false);
-
+  
   // haetaan kaikki tiedot
   const haeData = () => {
     fetch('https://restcountries.com/v3.1/all')
@@ -20,14 +19,14 @@ function App() {
   };
 
   //useEffect(() => haeData, []);
-  useEffect(() => setKaikki(jsondata), []);
+  useEffect(() => setKaikki(jsondata), []); // testausta varten, appi ei aina hakenut endpointista tietoja
 
   // aakkosta maat
   kaikki.sort((a, b) => a.name.common.localeCompare(b.name.common));
   
   // katsotaan mitä saatiin
-  console.log(JSON.stringify(kaikki));
-  console.log(kaikki.length); // 250
+  //console.log(JSON.stringify(kaikki));
+  //console.log(kaikki.length); // 250
 
   // maaolioiden lista
   const maat = kaikki.map((t, i) => {
@@ -37,6 +36,7 @@ function App() {
         nimi: t.name.common,
         paakaupunki: t.capital,
         itsenainen: (t.independent === true ? "Kyllä" : "Ei"),
+        yk: (t.unMember === true ? "Kyllä" : "Ei"),
         lippu: t.flag
       }
     )
@@ -50,7 +50,7 @@ function App() {
     //console.log("e.target.value on " + e.target.value);
     setHaettumaa(maat[e.target.value]);      
     setValmis(true);
-  } 
+  }  
    
   return (
     <div className="App" style={{ padding: "10px" }}>   
@@ -63,21 +63,25 @@ function App() {
       <br />
       <br />
 
-      {valmis ? 
+      {valmis ?   
+        
         <table align="center">
           <tbody>
-            <tr><th>Nimi</th><th>Pääkaupunki</th><th>Itsenäinen</th><th>Lippu</th></tr>           
+            <tr><th>Nimi</th><th>Pääkaupunki</th><th>Itsenäinen</th><th>YK:n jäsen</th><th>Lippu</th></tr>           
               <tr >             
                 <td>{haettumaa.nimi}</td>
                 <td>{haettumaa.paakaupunki}</td> 
                 <td>{haettumaa.itsenainen}</td> 
+                <td>{haettumaa.yk}</td> 
                 <td>{haettumaa.lippu}</td>               
               </tr>    
           </tbody>
-        </table>
-    : null}
+        </table>       
+        
+    : null}    
 
-    </div>
+    </div >
+      
   );
 }
 
